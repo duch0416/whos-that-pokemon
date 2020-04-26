d0eba5ba55e3e516358683bd5c4664cb30791d35
duch0416 <mikolajduchewicz@gmail.com>
2020-04-26 14:05:36 +0200

    add GameInterface component

---
+++ src/features/guessPokemon/components/GameInterface.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+`
+
+export interface GameInterfaceProps {
+    
+}
+ 
+const GameInterface: React.SFC<GameInterfaceProps> = () => {
+    return ( 
+        <Wrapper></Wrapper>
+     );
+}
+ 
+export default GameInterface;
--- src/features/guessPokemon/components/PokImg.tsx
+++ src/features/guessPokemon/components/PokImg.tsx
@@ -1,27 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-width: 100%;
-height: 75%;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 75%;
+`;
 const Img = styled.img`
-    max-width: 250px;
-`
+  max-width: 250px;
+`;
 
 export interface PokImgProps {
-    pokId: number | undefined;
+  pokId: number | undefined;
 }
- 
-const PokImg: React.SFC<PokImgProps> = ({pokId}) => {
-    return ( 
-        <Wrapper>
-            {pokId && <Img src={`https://pokeres.bastionbot.org/images/pokemon/${pokId}.png`} alt="pokemon"/>}
-        </Wrapper>
-     );
-}
- 
+
+const PokImg: React.SFC<PokImgProps> = ({ pokId }) => {
+  return (
+    <Wrapper>
+      {pokId && (
+        <Img
+          src={`https://pokeres.bastionbot.org/images/pokemon/${pokId}.png`}
+          alt="pokemon"
+        />
+      )}
+    </Wrapper>
+  );
+};
+
 export default PokImg;
